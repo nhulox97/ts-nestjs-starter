fc683ccbcf475e468c3595e003f3efa18aa7e14c
Sergio Bernal <sergiobernal909@gmail.com>
2021-07-19 11:01:47 -0600

    🔧 Add ignore and confs for eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'semi': 'warn',
+    'no-console': 'off',
+    'prettier/prettier': 'warn'
   },
 };
